df6422d5d63751eba49c11c50180a3b93bed1034
kan <kantasoworld0620@gmail.com>
2022-05-09 17:14:01 +0900

    factorization

--- python/app/others.py
+++ python/app/others.py
@@ -84,6 +84,29 @@ def sieve_of_eratosthenes(n: int) -> list:
     
     return P
 
+def factorization(n: int) -> list:
+    """nを素因数分解する関数
+
+    Args:
+        n (int): 素因数分解する数
+
+    Returns:
+        list: [素因数のリスト, 指数のリスト]
+    """
+    fact = []
+    for i in range(2, math.ceil(math.sqrt(n))):
+        if n % i == 0:
+            cnt = 0
+            while n % i == 0:
+                cnt += 1
+                n //= i
+            fact.append([i, cnt])
+
+    if n != 1:
+        fact[n] = 1
+
+    return fact
+
 if __name__ == "__main__":
     print("euler_tour")
     edges = [
