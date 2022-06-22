895f5ad7ae642d8a1e8e7bc40b87c815f71728b9
kan <kantasoworld0620@gmail.com>
2022-06-22 17:02:03 +0900

    factorization

--- cpp/multiset.cpp
+++ cpp/multiset.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 //二分探索木
-// NOTE 辞書を使ってうまくできないか考えてみる！
+//辞書とheapqを使ってうまくできないか考えてみる！
 //http://vivi.dyndns.org/tech/cpp/multiset.html
 int main(){
     //初期化
@@ -12,16 +12,23 @@ int main(){
     bt.insert(3);
     bt.insert(4);
     bt.insert(4);
+    bt.insert(4);
     bt.insert(7);
     //count
     cout << bt.count(3) << endl;
     cout << bt.count(4) << endl;
     // 削除
     int c = bt.erase(4);      //  4を全て削除、2 が返ってくる
+    cout << c << endl;
     // 値の検索
-    multiset<int>::iterator itr1 = bt.lower_bound(8);   // 2以上の最初の要素へのイテレータを返す
+    multiset<int>::iterator itr1 = bt.lower_bound(5);   // 2以上の最初の要素へのイテレータを返す
     cout << *itr1 << endl;
     multiset<int>::iterator itr2 = bt.upper_bound(2);   // 2より大きい最初の要素へのイテレータを返す
-    cout << *itr2 << endl;
-    
+    cout << *itr2 << endl;  
+    iterator itr = itr2 - bt.begin();
+    size_t index = distance(bt.begin(), itr1);
+    cout << index << endl;// 
+    cout << *bt.begin() << endl;// 最小値取得
+    //multiset<int>::iterator itr4 = bt.rbegin(); 
+    cout << *bt.rbegin() << endl; // 最大値取得
 }
